26ead55140e0262697da82c6f2bca9a8f586a0b5
Soft-Bunny-X <nickchenxuanli@outlook.com>
2025-03-28 08:39:18 -0400

    user logout, reset password, authentication token implemented and tested

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const asyncHandler = require("express-async-handler");
 const {body, validationResult} = require("express-validator");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
 const redis = require("redis");
 const redisClient = redis.createClient({
@@ -58,10 +59,92 @@ function sendVerificationCode(emailAddress, codeToSend){
     });
 }
 
+exports.user_get = [
+    asyncHandler(async (req, res, next) => {
+        // obtain the userID in cookie if there is any
+        const userID = req.cookies.userID;
+        // if no userID fetched, render the public user page, otherwise, redirect to the user specific page
+        if(!userID){
+            //====================
+            // console.log(`no userID found in cookies`);
+            //====================
+            res.render("user", {title: "User Center - Chenxuan's Personal Website"});
+        }
+        else if(!await redisClient.get(userID)){
+            res.render("user", {title: "User Center - Chenxuan's Personal Website"});
+        }
+        else{
+            res.redirect(`/user/main/${userID}`);
+        }
+    }),
+];
+
+exports.user_main_get = [
+    asyncHandler(async (req, res, next) => {
+        // obtain userID and authentication token from the cookies
+        const token = req.cookies.authToken;
+        const userID = req.cookies.userID;
+        // if either is missing, render the public user page
+        if(!token || !userID){
+            //====================
+            // console.log("token or userID not found from cookies");
+            //====================
+            res.redirect("/user");
+        }
+        else{
+            // otherwise, verify the account. Render the public user page if secretKey not found or if verification fails
+            const secretKey = await redisClient.get(userID);
+            //====================
+            // console.log(`\nsecretKey obtained from redis server is: ${secretKey}\n`);
+            //====================
+            if(!secretKey){
+                //====================
+                // console.log("secretKey not found from redis server");
+                // console.log(`\nID obtained is:`)
+                // console.log(userID);
+                //====================
+                res.redirect("/user");
+            }
+            else{
+                try{
+                    // This part renders the user specific page, not completed yet
+                    const decoded = jwt.verify(token, secretKey);
+                    const userTemporary = Users.find({userID: req.params.userID});
+                    res.render("user-main", {title: "User Center - Chenxuan's User"});
+                } catch(error){
+                    console.log("token verification fails");
+                    res.redirect("/user");
+                }
+            }
+        }
+    }),
+];
+
+exports.user_log_out_get = [
+    asyncHandler(async (req, res, next) => {
+        const userID = req.cookies.userID;
+
+        if(!userID){
+            res.redirect("/user");
+        }
+        else{
+            // if(!await redisClient.del(userID)){
+            //     res.redirect("/user");
+            // }
+            // else{
+            //     res.redirect("/user");
+            // }
+            await redisClient.del(userID)
+            res.redirect("/user");
+        }
+    }),
+];
+
 exports.user_sign_up_post = [
     // validate and sanitize the inputs
     body("email")
         .trim()
+        .toLowerCase()
         .isLength({min: 1})
         .escape()
         .withMessage("email must be filled")
@@ -111,32 +194,6 @@ exports.user_sign_up_post = [
                 EX: 600
             });
             
-            // const mailOptions = {
-            //     from: "chenxuanli.official@gmail.com",
-            //     to: req.body.email,
-            //     subject: "Sign Up Email Verification - Chenuxan Li's Personal Website",
-            //     html: `
-            //         <h1>Thank you for joining Chenxuan's Personal Website!</h1>
-            //         <p> Greetings, <br><br></p>
-            //         <p> Your verification code is:<br></p>
-            //         <h2 style="margin: 10px; padding: 10px; background-color: #DDD; color: #A22; border-radius: 5px; text-align: center;">${verificationCode}</h2>
-            //         <p> <br> Please ignore this email if you did not sign up on Chenxuan's Personal Website. </p>
-            //         <p> Best,</p>
-            //         <p> Chenxuan Li</p>
-            //         <div style="magin: 10px; padding: 10px; background-color: #DDD;">
-            //             <a href="mailto:nickchenxuanli@outlook.com" style="text-decoration: none;">nickchenxuanli@outlook.com</a>
-            //         </div>
-            //     `
-            // }
-            
-            // transporter.sendMail(mailOptions, (error, info) => {
-            //     if(error) {
-            //         console.log("Error: ", error);
-            //     }
-            //     else{
-            //         console.log("Email sent: ", info.response);
-            //     }
-            // });
             sendVerificationCode(req.body.email, verificationCode);
 
             res.redirect(`/user/sign-up/verification/${req.body.email}`);
@@ -169,7 +226,7 @@ exports.user_sign_up_verification_post = [
         // if any error occured, re-render the sign up page with the errors
         // otherwise, create the user's credential and redirect to the user's main page
         if(!errors.isEmpty()){
-            res.render("user-sign-up-verify", {
+            res.render("user-verify", {
                 title: "Email Verification - Chenxuan's User",
                 errors: errors.array()
             });
@@ -188,7 +245,7 @@ exports.user_sign_up_verification_post = [
                 conversation: [],
             });
             await newUser.save();
-            res.redirect(`/user/${newCredential._id}`);
+            res.redirect(`/user/main/${newCredential._id}`);
         }
     }),
 ];
@@ -197,6 +254,7 @@ exports.user_log_in_post = [
     // validate and sanitize the inputs
     body("email")
         .trim()
+        .toLowerCase()
         .isLength({min: 1})
         .escape()
         .withMessage("email must be filled")
@@ -238,15 +296,131 @@ exports.user_log_in_post = [
             });
         }
         else{
-            res.redirect(`/user/${credentialTemporary._id}`);
+            // create a 8-byte random number and express it in hexadecimal string
+            const secretKey = crypto.randomBytes(4).toString("hex");
+            // save the user specific key in the memory
+            //====================
+            // console.log(userTemporary.userID);
+            // console.log(secretKey);
+            //====================
+            await redisClient.set(userTemporary.userID.toString(), secretKey, {EX: 604800});
+            //====================
+            // console.log(`\nuserID set: ${userTemporary.userID.toString()}\n`);
+            // console.log(`\nsecretKey set: ${secretKey}\n`);
+            //====================
+            // sign the token with the user specific key
+            const token = jwt.sign({userID: userTemporary.userID.toString()}, secretKey, {expiresIn: "7d"});
+            // set cookie with authentication token
+            res.cookie("authToken", token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 604800000
+            });
+
+            res.cookie("userID", userTemporary.userID.toString(), {
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 604800000
+            });
+
+            res.redirect(`/user/main/${credentialTemporary._id}`);
         }
     }), 
 ];
 
-exports.user_main_get = [
+exports.user_recovery_post = [
+    // validate and sanitize the inputs
+    body("email")
+        .trim()
+        .toLowerCase()
+        .escape()
+        .isEmail()
+        .withMessage("Invalid email input"),
+    body("password")
+        .trim()
+        .isLength({min: 8})
+        .escape()
+        .withMessage("password must contain at least 8 characters"),
+    // handle the inputs
     asyncHandler(async (req, res, next) => {
-        const userTemporary = Users.find({userID: req.params.userID});
-        res.render("user-main", {title: "User Center - Chenxuan's User"});
-        // res.render("not found");
+        const errors = validationResult(req);
+        // test if the email is on file
+        if(!await Users.exists({email: req.body.email})){
+            errors.errors.push({
+                msg: "User does not exists!",
+                param: "email",
+                location: "body"
+            });
+            console.log("user doesn't exists");
+        }
+        // if any error occured, re-render the recovery page with the errors
+        // otherwise, redirect to the verification page
+        if(!errors.isEmpty()){
+            res.render("user-recovery", {
+                title: "Account Recovery - Chenxuan's User",
+                errors: errors.array()
+            });
+        }
+        else{
+            const verificationCode = crypto.randomInt(100000, 999999).toString();
+
+            const userTemporaryProfile = {
+                email: req.body.email,
+                password: req.body.password,
+                code: verificationCode,
+            };
+
+            await redisClient.set(req.body.email, JSON.stringify(userTemporaryProfile), {
+                EX: 600
+            });
+
+            sendVerificationCode(req.body.email, verificationCode);
+
+            res.redirect(`/user/recovery/verification/${req.body.email}`);
+        }
+    }),
+];
+
+exports.user_recovery_verification_post = [
+    // validate and sanitize the inputs
+    body("code")
+        .trim()
+        .isLength({min: 6, max: 6})
+        .escape()
+        .withMessage("verification code must be 6-digit")
+        .isNumeric()
+        .withMessage("verification code must be numerical input"),
+    // handle the inputs
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        // get the verification code created earlier when the user submited the sign up form
+        const userTemporaryProfile = JSON.parse(await redisClient.get(req.params.email));
+        // test if no verification code on file (could due to expiration) or if the code doesn't match
+        if(userTemporaryProfile == null || userTemporaryProfile.code != req.body.code){
+            errors.errors.push({
+                msg: "Incorrect Verification Code!",
+                param: "code",
+                location: "body"
+            });
+        }
+        // if any error occured, re-render the sign up page with the errors
+        // otherwise, create the user's credential and redirect to the user's main page
+        if(!errors.isEmpty()){
+            res.render("user-verify", {
+                title: "Email Verification - Chenxuan's User",
+                errors: errors.array()
+            });
+        }
+        else{
+            const recoveryUser = await Users.findOne({email: req.params.email});
+            const recoveryCredential = await Credentials.findById(recoveryUser.userID);
+            recoveryCredential.password = userTemporaryProfile.password;
+            console.log(recoveryCredential);
+            console.log(userTemporaryProfile);
+            await recoveryCredential.save();
+            res.redirect(`/user/main/${recoveryCredential._id}`);
+        }
     }),
 ];
--- routes/user.js
+++ routes/user.js
@@ -4,28 +4,44 @@ const user_controller = require("../controllers/userController");
 const gallery_controller = require("../controllers/galleryController");
 
 /* GET users listing. */
-router.get("/", (req, res, next) => {
-  res.render("user", {title: "User Center - Chenxuan's Personal Website"});
-});
+router.get("/", user_controller.user_get);
+
+router.get("/main/:userID", user_controller.user_main_get);
+
+router.get("/log-out", user_controller.user_log_out_get);
 
 router.get("/sign-up", (req, res, next) => {
-  res.render("user-sign-up", {title: "Sign Up - Chenxuan's User"})
+  res.render("user-sign-up", {title: "Sign Up - Chenxuan's User"});
 });
 
 router.post("/sign-up", user_controller.user_sign_up_post);
 
 router.get("/sign-up/verification/:email", (req, res, next) => {
-  res.render("user-sign-up-verify", {title: "Email Verification - Chenxuan's User"});
+  res.render("user-verify", {title: "Email Verification - Chenxuan's User"});
 });
 
 router.post("/sign-up/verification/:email", user_controller.user_sign_up_verification_post);
 
 router.get("/log-in", (req, res, next) => {
-  res.render("user-log-in", {title: "Log In - Chenxuan's User"})
+  res.render("user-log-in", {title: "Log In - Chenxuan's User"});
 });
 
 router.post("/log-in", user_controller.user_log_in_post);
 
-router.get("/:userID", user_controller.user_main_get);
+router.get("/recovery", (req, res, next) => {
+  res.render("user-recovery", {title: "Account Recovery - Chenxuan's User"});
+});
+
+router.post("/recovery", user_controller.user_recovery_post);
+
+// router.get("/recovery/verification/:email", (req, res, next) => {
+//   res.render("user-verify", {title: "Email Verification - Chenxuan's User"});
+// });
+
+router.get("/recovery/verification/:email", (req, res, next) => {
+  res.render("user-verify", {title: "Email Verification - Chenxuan's User"});
+});
+
+router.post("/recovery/verification/:email", user_controller.user_recovery_verification_post);
 
 module.exports = router;
